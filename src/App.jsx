f96ba96f313b2bdc321b27701c0d066362615e12
al1yew <vasifja@code.edu.az>
2023-05-04 17:12:30 +0400

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,15 @@ import Form from "./Form";
 import { ToastContainer, toast } from "react-toastify";
 import Items from "./Items";
 const App = () => {
-   const [items, setItems] = useState([]);
+    const [items, setItems] = useState([]);
 
-   return (
-      <section className="section-center">
-         <Form setItems={setItems} />
-         <Items items={items} setItems={setItems} />
-         <ToastContainer autoClose={1000} />
-      </section>
-   );
+    return (
+        <section className="section-center">
+            <Form setItems={setItems} />
+            <Items items={items} setItems={setItems} />
+            <ToastContainer autoClose={1000} />
+        </section>
+    );
 };
 
 export default App;
--- src/Items.jsx
+++ src/Items.jsx
@@ -3,37 +3,40 @@ import SingleItem from "./SingleItem";
 import { toast } from "react-toastify";
 
 const Items = ({ items, setItems }) => {
-   const removeItem = (id) => setItems(items.filter((x) => x.id !== id));
+    const removeItem = (id) => setItems(items.filter((x) => x.id !== id));
 
-   const submitUpdate = useCallback(({ id, updatedValue }) => {
-      const updatedItems = items.map((item) => {
-         if (item.id === id) {
-            //esli eto tot samiy item, menayem ego
-            return { ...item, name: updatedValue };
-         }
-         return item; //esli eto ne tot item, to prosto proxodim mimo i vozvrashayem. Map metod ne dayet nam noviy array
-      });
+    const submitUpdate = useCallback(({ id, updatedValue }) => {
+        const updatedItems = items.map((item) => {
+            if (item.id === id) {
+                //esli eto tot samiy item, menayem ego
+                return {
+                    ...item,
+                    name: updatedValue.length > 0 ? updatedValue : item.name,
+                };
+            }
+            return item; //esli eto ne tot item, to prosto proxodim mimo i vozvrashayem. Map metod ne dayet nam noviy array
+        });
 
-      setItems(updatedItems);
+        setItems(updatedItems);
 
-      toast.success("Updated");
-   });
+        toast.success("Updated");
+    });
 
-   //esli ne sdelat usecallback, App.jsx renderitsa ved nash state naxoditsa tam, a s pomosyu callback mi predotvratili render
+    //esli ne sdelat usecallback, App.jsx renderitsa ved nash state naxoditsa tam, a s pomosyu callback mi predotvratili render
 
-   return (
-      <div className="items">
-         {items?.map((item) => {
-            return (
-               <SingleItem
-                  key={item.id}
-                  submitUpdate={submitUpdate}
-                  removeItem={removeItem}
-                  item={item}
-               />
-            );
-         })}
-      </div>
-   );
+    return (
+        <div className="items">
+            {items?.map((item) => {
+                return (
+                    <SingleItem
+                        key={item.id}
+                        submitUpdate={submitUpdate}
+                        removeItem={removeItem}
+                        item={item}
+                    />
+                );
+            })}
+        </div>
+    );
 };
 export default Items;
